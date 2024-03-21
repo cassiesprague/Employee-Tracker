163c87f2d266b6abfe180fccec4d1308c71f16ad
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-21 17:23:02 -0400

    adding answers to server.js

--- server.js
+++ server.js
@@ -15,16 +15,23 @@ const db = mysql.createConnection(
 
 //Below shows my first question that will show when running the app
 inquirer
-.prompt(
+.prompt([
     {
         type: "list",
         name: "options",
         message: "What would you like to do?",
         choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Update employee manager", "View employees by manager", "View employees by department", "Delete departments", "Delete roles", "Delete employees", "View the total utilized budget of department", "Exit",],
-    }
-)
+    },
+])
 
 //Below shows an arrow function that creates the const for choices
-.then((answers) => {
-    const {choices} = answers;
+//Used the following link figure out correct way to get answers formatted 
+//https://stackoverflow.com/questions/71534705/inquirer-exits-before-i-am-able-to-make-a-choice-in-node-js
+//https://javascript.plainenglish.io/how-to-inquirer-js-c10a4e05ef1f
+.then((answer) => {
+    if (answer === "View all departments"){
+        viewDepartments()
+    } else if (answer === "View all roles"){
+        viewRoles()
+    }
 })
