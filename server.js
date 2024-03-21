fbf2216fd4534b2a174cd8a4d60194a801ba41e3
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-21 15:12:59 -0400

    added answer portion in server.js and comments

--- server.js
+++ server.js
@@ -10,5 +10,21 @@ const db = mysql.createConnection(
         password: '',
         database: 'tracker_db'
     },
-    console.log(`Connected to the tracker_db database.`)
+    console.log(`Connected to the tracker_db database!`)
 );
+
+//Below shows my first question that will show when running the app
+inquirer
+.prompt(
+    {
+        type: "list",
+        name: "options",
+        message: "What would you like to do?",
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Update employee manager", "View employees by manager", "View employees by department", "Delete departments", "Delete roles", "Delete employees", "View the total utilized budget of department", "Exit",],
+    }
+)
+
+//Below shows an arrow function that creates the const for choices
+.then((answers) => {
+    const {choices} = answers;
+})
