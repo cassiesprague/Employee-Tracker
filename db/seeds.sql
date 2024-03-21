3b8ca5ea10038accbd63e658ed20ec071cb765d6
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-21 14:35:03 -0400

    added seeds.sql to insert in tables in schema.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,6 +8,7 @@ VALUES  ("Reception"),
 
 INSERT INTO role (title, salary, department_id)
 VALUES  ("Receptionist", 30000, 1),
+        ("Regional Manager", 49000, 2),
         ("Co-Manager", 45000, 2),
         ("Assistant to the Regional Manager,", 40000, 2),
         ("Salesman", 35000, 2),
@@ -19,4 +20,19 @@ VALUES  ("Receptionist", 30000, 1),
         ("Human Resources Representative", 32000, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ()
+VALUES  ("Pam", "Beesley", 1, 2),
+        ("Michael", "Scott", 2, null),
+        ("Jim", "Halpert", 3, 2),
+        ("Dwight", "Schrute", 4, 2),
+        ("Stanley", "Hudson", 5, 2),
+        ("Phyllis", "Vance", 5, 2),
+        ("Creed", "Bratton", 9, 2),
+        ("Andy", "Bernard", 5, 2),
+        ("Meredith", "Palmer", 10, 2),
+        ("Angela", "Martin", 6, 2),
+        ("Oscar", "Martinez", 7, 2),
+        ("Kevin", "Malone", 7, 2),
+        ("Kelly", "Kapoor", 8, 2),
+        ("Toby", "Flenderson", 11, 2);
+
+
