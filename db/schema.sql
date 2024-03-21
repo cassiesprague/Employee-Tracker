950a3bc5dac7d496fa8fe011f8e88781513c9f65
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-21 16:39:26 -0400

    added auto increment after int primary key and schanged if from name to department_name since name would not work

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,19 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(15) NOT NULL,
     department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (name)
+INSERT INTO department (department_name)
 VALUES  ("Reception"),
         ("Sales"),
         ("Accounting"),
